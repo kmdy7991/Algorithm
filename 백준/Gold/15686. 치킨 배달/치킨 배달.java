d4185afe9fc8f123e4a598fece6f08468fcd61a8
DY <108215226+kmdy7991@users.noreply.github.com>
2024-08-29 13:44:47 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -4,61 +4,67 @@ import java.util.function.Function;
 
 public class Main {
 	static Function<String, Integer> stoi = Integer::parseInt;
-	static int n, m, dis;
-	static int[][] map;
-	static List<int[]> p = new ArrayList<>();
-	static List<int[]> c = new ArrayList<>();
-	
-	public static void main(String[] args) throws IOException {
-		input();
-	}
+	static int n, m, res;
+	static List<int[]> chicken;
+	static List<int[]> houses;
 
-	private static void input() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
 
 		n = stoi.apply(st.nextToken());
 		m = stoi.apply(st.nextToken());
-		map = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			map[i] = Arrays.stream(br.readLine().split(" "))
-					       .mapToInt(stoi::apply).toArray();
-		}
+
+		houses = new ArrayList<>();
+		chicken = new ArrayList<>();
 
 		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine());
+
 			for (int j = 0; j < n; j++) {
-				if (map[i][j] == 1) p.add(new int[] { i, j });
-				if (map[i][j] == 2) c.add(new int[] { i, j });
+				int num = stoi.apply(st.nextToken());
+
+				if (num == 1) {
+					houses.add(new int[] { i + 1, j + 1 });
+				} else if (num == 2) {
+					chicken.add(new int[] { i + 1, j + 1 });
+				}
 			}
 		}
-        
-		dis = (int) 1e9;
-		survive(0, 0, new int[m]);
-		bw.write(dis+"");
-        bw.close();
+
+		res = (int) 2e9;
+		findDistance(0, 0, new int[m]);
+		System.out.println(res);
 	}
 
-	private static void survive(int cnt, int idx, int[] p) {
+	private static void findDistance(int idx, int cnt, int[] store) {
 		if (cnt == m) {
-			dis = Math.min(dis, getDis(p));
+			res = Math.min(minPath(store), res);
 			return;
 		}
-		for (int i = idx; i < c.size(); i++) {
-			p[cnt] = i;
-			survive(cnt + 1, i + 1, p);
+
+		for (int i = idx; i < chicken.size(); i++) {
+			store[cnt] = i;
+			findDistance(i + 1, cnt + 1, store);
 		}
 	}
 
-	private static int getDis(int[] chicken) {
-		int tmp = 0;
-		for (int[] h : p) {
-			int current = (int) 1e9;
-			for (int ch : chicken) {
-				current = Math.min(current, Math.abs(h[0] - c.get(ch)[0]) + Math.abs(h[1] - c.get(ch)[1]));
+	private static int minPath(int[] store) {
+		int min = 0;
+
+		for (int[] house : houses) {
+			int sum = (int) 2e9;
+			
+			for (int idx : store) {
+				sum = Math.min(sum, path(house, chicken.get(idx)));
 			}
-			tmp += current;
+
+			min += sum;
 		}
-		return tmp;
+		return min;
+	}
+
+	private static int path(int[] h, int[] c) {
+		return Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
 	}
 }
