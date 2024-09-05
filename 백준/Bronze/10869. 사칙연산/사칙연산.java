efc4787686054895b8a90e70fcc15b3a541f75ae
DY <108215226+kmdy7991@users.noreply.github.com>
2024-09-05 19:37:07 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        
+        int a = stoi.apply(st.nextToken());
+        int b = stoi.apply(st.nextToken());
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+	}
+}
