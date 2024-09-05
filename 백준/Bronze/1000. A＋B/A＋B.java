fef103c6318012d418e9b327db5f7c9d4bb9f194
DY <108215226+kmdy7991@users.noreply.github.com>
2024-09-05 19:32:29 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        
+        System.out.print(stoi.apply(st.nextToken()) + stoi.apply(st.nextToken()));        
+	}
+}
