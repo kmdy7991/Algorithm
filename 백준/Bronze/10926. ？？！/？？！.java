00465d9de95c1efbf73eefb8584d5d0933d9c73a
DY <108215226+kmdy7991@users.noreply.github.com>
2024-09-05 19:39:31 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(in.readLine() + "??!");
+    }
+}
