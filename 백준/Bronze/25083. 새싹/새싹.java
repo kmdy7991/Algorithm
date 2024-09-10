d652e0b534c27cfe50cd598debec7207ae035e75
DY <108215226+kmdy7991@users.noreply.github.com>
2024-09-10 23:54:33 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
