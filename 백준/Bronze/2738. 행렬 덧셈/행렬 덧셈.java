9d45ff02c5f090a27a2d6b3a84278370b08f6e25
DY <108215226+kmdy7991@users.noreply.github.com>
2024-09-11 18:04:27 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 292 ms, Memory: 19140 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		int n = stoi.apply(st.nextToken());
+		int m = stoi.apply(st.nextToken());
+
+		int[][] a = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < m; j++) {
+				a[i][j] = stoi.apply(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < m; j++) {
+				a[i][j] += stoi.apply(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
