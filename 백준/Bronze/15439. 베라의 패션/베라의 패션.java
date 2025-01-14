5b2d4a281fee2f8e5ade479b9c92981bda90c4ea
DY <108215226+kmdy7991@users.noreply.github.com>
2025-01-14 17:15:08 +0900

    [Bronze IV] Title: 베라의 패션, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+    static Function<String, Integer> stoi = Integer::parseInt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = stoi.apply(in.readLine());
+
+        System.out.println(n * (n - 1));
+    }
+}
