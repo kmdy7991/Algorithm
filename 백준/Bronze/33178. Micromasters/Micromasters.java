5ee9ce0d7570544c75c5d63a9d0cf0d7c5342f2a
DY <108215226+kmdy7991@users.noreply.github.com>
2025-02-13 12:15:25 +0900

    [Bronze V] Title: Micromasters, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(in.readLine());
+
+        System.out.println(n / 10);
+    }
+}
