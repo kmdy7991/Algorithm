4b2121c55d46d85dab9075cb83b1598e42ea0a65
DY <108215226+kmdy7991@users.noreply.github.com>
2025-03-24 02:17:14 +0900

    [D1] Title: 대각선 출력하기, Time: 154 ms, Memory: 58,512 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Solution {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.print("++++#");
+	}
+}
