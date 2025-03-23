56415a9e73955431584ee150d01ea9fc094eb594
DY <108215226+kmdy7991@users.noreply.github.com>
2025-03-24 01:50:08 +0900

    [D1] Title: 신문 헤드라인, Time: 128 ms, Memory: 58,316 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/README.md
@@ -0,0 +1,15 @@
+# [D1] 신문 헤드라인 - 2047 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq) 
+
+### 성능 요약
+
+메모리: 58,316 KB, 시간: 128 ms, 코드길이: 22 Bytes
+
+### 제출 일자
+
+2022-10-23 21:50
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.py
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class Solution {
+	static Function<String, Integer> stoi = Integer::parseInt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println(in.readLine().toUpperCase());
+	}
+}
