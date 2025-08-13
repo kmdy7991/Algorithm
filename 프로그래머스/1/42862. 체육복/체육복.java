91679ae47b5716b1e65559c2f1824510cedf2c95
DY <108215226+kmdy7991@users.noreply.github.com>
2025-08-13 21:25:51 +0900

    [level 1] Title: 체육복, Time: 1.24 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,6 +1,34 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int answer = n - lost.length;
+        
+        
+        boolean[] check = new boolean[31];
+        
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        
+        
+        for (int num : lost) {
+            for(int give : reserve) {
+                if (!check[give] && give == num) {
+                    answer++;
+                    check[give] = true;
+                    break;
+                }
+                
+                if(!check[give] && !check[num] && (num - 1 == give || num + 1 == give)) {
+                    check[give] = true;
+                    check[num] = true;
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
         return answer;
     }
 }
